1950eb406b34048f75468434e3ec65624488c6be
soural1417 <soural1417@gmail.com>
2020-06-13 12:46:39 +0530

    first

---
+++ blog/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
+from . import views
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='blog-about'),
+    path('workspace/', views.workspace, name='blog-workspace'),
+    path('groups/', views.group, name='blog-group'),
+    path('premium/', views.premium, name='blog-premium'),
+    path('support/', views.support, name='blog-support'),
+]
